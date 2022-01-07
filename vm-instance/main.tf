b50bd719b1ecb8c56770964defe3a6ba8886a819
war <war@wars-MBP-2.attlocal.net>
2022-01-07 15:55:01 -0800

    add final terraform scripts with automated bash scripts

--- vm-instance/main.tf
+++ vm-instance/main.tf
@@ -1,12 +1,28 @@
 # Provider type
 provider "google" {
     project     = "${var.project_name}"
-    region      = "${var.aws_region}"
-    credentials = "${file("${var.ssh-key}/apigee-test-334219.json")}"
+    region      = "${var.region}"
+    credentials = "${file("${var.path}/apigee-test-334219.json")}"
 }
 
 # Compute instance 
 resource "google_compute_instance" "default" {
-    name = ""
+    name = "meta-vm"
+    machine_type = "n1-standard-1"
+    zone = "us-west2-a"
+
+    boot_disk {
+      initialize_params {
+          image = "ubuntu-os-cloud/ubuntu-1804-lts"
+      }
+    }
+
+    network_interface {
+      network = "default"
+    }
+
+    service_account {
+        scopes = ["cloud-platform"]    
+    }
 
 }
--- vm-instance/terraform.tfvars
+++ vm-instance/terraform.tfvars
@@ -1,3 +1,3 @@
 project_name = "apigee-test-334219"
-aws-region = "us-west-2"
-path = "~/.config/gcloud/apigee-test-334219.json" 
+region = "us-west2"
+path = "~/.config/gcloud/" 
